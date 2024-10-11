15c475609632baab3d51d707bb52ffc1a828efad
DanielaEsposito <esposito.daniela9817@gmail.com>
2024-10-11 14:30:13 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const numOne = 8; // NON MODIFICARE QUESTA RIGA
+const numTwo = 16; // NON MODIFICARE QUESTA RIGA
+const numThree = 32; // NON MODIFICARE QUESTA RIGA
+
+// Sostituisci NaN di seguito utilizzando
+// gli operatori numerici di Javascript
+
+// 1. Imposta questa variabile come somma di numOne e numTwo
+const result = numOne + numTwo;
+console.log(result); // Output: 24
+
+// 2. Imposta questa variabile come prodotto di numThree e numTwo
+const  product = numTwo * numThree;
+console.log(product); // Output: 512
+
+// 3. Imposta questa variabile come divisione di numThree per numOne
+const division = numThree / numOne;
+console.log(division); // Output: 4
+
+// 4. Imposta questa variabile come differenza tra numThree e numOne
+const subt = numThree - numOne;
+console.log(subt); // Output: 4
+
+// 5. Imposta questa variabile come somma di numOne, numTwo e numThree
+const sum = numOne + numTwo + numThree;
+console.log(sum); // Output: 56
+
+// 6. Imposta questa variabile come somma di numOne, numTwo, numThree diviso numOne
+const numBytes = (numOne + numTwo +numThree) / numOne;
+console.log(numBytes); // Output: 7
